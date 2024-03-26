6c11a4a3b6a80081dd3b03e08649e7ff97464e63
杨枫 <10992511+yangfeng2017@user.noreply.gitee.com>
2024-03-26 17:57:42 +0800

    修改3d模型

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,20 +1,18 @@
 <!-- @format -->
 
 <template>
-  <!-- <Model></Model> -->
-  <Crosswise-scroll>
+  <Model></Model>
+  <Crosswise-scroll @startRenderDom="startRenderDom">
     <div class="scrollBox"><Welcome></Welcome></div>
     <div class="scrollBox"><work v-if="flagRand"></work></div>
-    <div class="scrollBox"><project v-if="flagRand"></project></div>
     <div class="scrollBox"><Sam v-if="flagRand"></Sam></div>
     <div class="scrollBox"><imgWorks v-if="flagRand"></imgWorks></div>
   </Crosswise-scroll>
   <div class="navBoxList" @click="startRenderDom">
     <navBox index="1">首页</navBox>
-    <navBox index="2">工作</navBox>
-    <navBox index="3">项目</navBox>
-    <navBox index="4">SAM</navBox>
-    <navBox index="5">卡片</navBox>
+    <navBox index="2">经历</navBox>
+    <navBox index="3">SAM</navBox>
+    <navBox index="4">卡片</navBox>
   </div>
 </template>
 
@@ -26,12 +24,11 @@ import imgWorks from "@/components/imgWorks/index.vue";
 import CrosswiseScroll from "@/components/crosswiseScroll/index.vue";
 import Welcome from "@/components/Welcome/index.vue";
 import work from "@/components/work/index.vue";
-import project from "@/components/project/index.vue";
 import { ref } from "vue";
 // 优化首屏加载 避免首页animation动画卡顿
 let flagRand = ref<boolean>(false);
 function startRenderDom() {
- flagRand.value = true;
+  flagRand.value = true;
 }
 </script>
 
